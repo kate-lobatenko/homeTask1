8cbe8a38ffd0f3214128c1f9c7dd917d3433f184
kate-lobatenko <ttt1232444@gmail.com>
2019-03-02 23:00:58 +0200

    updates for index.html, portolio.html and styles for them

--- js/main.js
+++ js/main.js
@@ -1,48 +1,56 @@
 $(document).ready(function () {
-    $('.album-banner').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        arrows: true,
-        accessibility: true
-    });
+  // Banners and slider initialisation
+  $('.album-banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    accessibility: true,
+    dots: true
+  });
 
-    $('.clients-slider').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: true,
-        accessibility: true
-    });
+  $('.clients-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    accessibility: true
+  });
 
-    $('.our-jobs-slider').slick({
-        centerMode: true,
-        centerPadding: '0',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        accessibility: true,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+  $('.our-jobs-slider').slick({
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    accessibility: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  // Banners and slider initialisation end
+
+  // Add active for links on header's navbar
+  $('.navigation li').click(function () {
+    $('.navigation li').not(this).removeClass('active');
+    $(this).toggleClass('active');
+  });
 });
