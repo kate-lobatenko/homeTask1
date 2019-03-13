c22cc634c564bb540c789f76826e24b34581ae68
kate-lobatenko <ttt1232444@gmail.com>
2019-03-13 21:36:26 +0200

    add js and styles for mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 
+
+  $(window).resize(function () {
+    controlMbLinks();
+  });
+
   // Add current year to footer
   let currentYear = (new Date).getFullYear();
   $('#year').text(currentYear);
@@ -83,34 +88,56 @@ $(document).ready(function () {
   $('.hamburger').click(function () {
     $(this).toggleClass('is-active');
     $('#navigation').toggleClass('open');
+    controlMbLinks();
+ 
   });
 
+  $('.open-menu-link.open.icon-angle-down').keyup(function () {
+    $('.parent.active .second-level').addClass('show');
+  });
+
+  // Check if mobile and add/remove buttons in mobile menu
+  function controlMbLinks() {
+    if (!($('.open-menu-link').length) && ($(window).width() <= 991)) {
+      $('.parent').append("<div class='open-menu-link open icon-angle-down'></div>");
+      $('.parent').append("<div class='open-menu-link close icon-angle-right hidden'></div>");
+    } else {
+      $('.open-menu-link').remove();
+    }
+  }
+
   // Progress bar
-  $(".progress-bar").each(function(){
+  $(".progress-bar").each(function () {
     var bar = $(this).find(".bar");
     var val = $(this).find("span");
-    var per = parseInt( val.text(), 10);
+    var per = parseInt(val.text(), 10);
     var $right = $('.right');
     var $back = $('.back');
-  
-    $({p:0}).animate({p:per}, {
+
+    $({
+      p: 0
+    }).animate({
+      p: per
+    }, {
       duration: 3000,
-      step: function(p) {
+      step: function (p) {
         bar.css({
-          transform: "rotate("+ (45+(p*1.8)) +"deg)"
+          transform: "rotate(" + (45 + (p * 1.8)) + "deg)"
         });
-        val.text(p|0);
+        val.text(p | 0);
       }
-    }).delay( 200 );
-    
+    }).delay(200);
+
     if (per == 100) {
-       $back.delay( 2600 ).animate({'top': '18px'}, 200 );
+      $back.delay(2600).animate({
+        'top': '18px'
+      }, 200);
     }
-        
+
     if (per == 0) {
       $('.left').css('background', 'gray');
     }
-      
+
   });
 
 });
